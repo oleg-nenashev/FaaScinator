657c016d92ed4ed8b9028a7e21bda28b1b6fb93f
Oleg Nenashev <o.v.nenashev@gmail.com>
2021-07-04 08:08:19 +0200

    Generate help files using picocli-codegen and Asciidoctor

--- function/3_service/src/main/java/io/faascinator/service/DocResource.java
+++ function/3_service/src/main/java/io/faascinator/service/DocResource.java
@@ -1,12 +1,25 @@
 package io.faascinator.service;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.faascinator.service.util.PicocliExtractor;
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Options;
+import org.asciidoctor.jruby.AsciiDocDirectoryWalker;
+import org.asciidoctor.jruby.internal.JRubyAsciidoctor;
+import picocli.CommandLine;
 import picocli.codegen.docgen.manpage.ManPageGenerator;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 /**
  * Generates documentation, powered by {@link picocli.codegen.docgen.manpage.ManPageGenerator}.
@@ -18,10 +31,45 @@ public class DocResource extends FaaScinatorResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String generateDoc() throws IOException {
-        ManPageGenerator manGenerator = new ManPageGenerator();
-        manGenerator.
-
-        return "";
+    @Path("")
+    public String generateRootDoc() throws IOException {
+        return generateDoc(PicocliExtractor.extractCommandLine(config), false);
     }
+
+    // TODO: Add support for nested sub-commands
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{subcommand}")
+    public String generateSubcommandDoc(@PathParam("subcommand") String subcommand) throws IOException {
+        final CommandLine commandLine = PicocliExtractor.extractCommandLine(config);
+        final CommandLine subcommandHandler = commandLine.getSubcommands().get(subcommand);
+        if (subcommandHandler == null) {
+            throw new NotFoundException("Subcommand does not exist: " + subcommand);
+        }
+        return generateDoc(subcommandHandler, true);
+    }
+
+    @SuppressFBWarnings(value = "PATH_TRAVERSAL_IN", justification = "Sanitized by CommandLine")
+    public String generateDoc(CommandLine commandLine, boolean isSubcommand) throws IOException {
+        final Asciidoctor asciidoctor = new JRubyAsciidoctor();
+        final File tmpDir = Files.createTempDirectory("faascinator-docs").toFile();
+
+        ManPageGenerator.generateManPage(tmpDir, null, new boolean[2], false, commandLine.getCommandSpec());
+
+        System.out.println("Converting man pages to HTML...");
+        String[] result = asciidoctor.convertDirectory(
+                new AsciiDocDirectoryWalker(tmpDir.getAbsolutePath()),
+                Options.builder().build());
+        if (result.length != 0) {
+            throw new BadRequestException("Generated empty HTML");
+        }
+
+        //TODO: Support nested subcommands
+        final File helpFile = isSubcommand
+                ? new File (tmpDir, commandLine.getParent().getCommandName() + "-" + commandLine.getCommandName() + ".html")
+                : new File (tmpDir, commandLine.getCommandName() + ".html");
+        //TODO: Switch to readString() in Java 11
+        return new String(Files.readAllBytes(helpFile.toPath()), StandardCharsets.UTF_8);
+    }
+
 }
